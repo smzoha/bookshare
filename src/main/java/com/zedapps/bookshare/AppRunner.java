3d474da28d4b19432c23542bd3f8a79823bbf7cc
Shamah M Zoha <shamah.zoha@gmail.com>
2025-09-06 18:35:40 +0600

    Initial Commit
    The commit includes the basic setup for a Spring Boot project. It
    includes the gitignore file, the build.gradle file containing all
    the dependencies, as well as the AppRunner class, which will allow
    the application to run. The gradle wrapper has been added as well for
    ease of deployment/build.
    
    The application is set to run on port 6001.

---
+++ src/main/java/com/zedapps/bookshare/AppRunner.java
@@ -0,0 +1,16 @@
+package com.zedapps.bookshare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author smzoha
+ * @since 9/6/25
+ */
+@SpringBootApplication
+public class AppRunner {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppRunner.class, args);
+    }
+}
