b5e5da5856790f66e83b4927797381444b6e5a68
Shamah M Zoha <shamah.zoha@gmail.com>
2025-10-13 19:59:43 +0600

    Implement Review Pagination

--- src/main/java/com/zedapps/bookshare/controller/book/BookController.java
+++ src/main/java/com/zedapps/bookshare/controller/book/BookController.java
@@ -3,9 +3,11 @@ package com.zedapps.bookshare.controller.book;
 import com.zedapps.bookshare.dto.book.BookReviewDto;
 import com.zedapps.bookshare.dto.book.ReviewLikeResponseDto;
 import com.zedapps.bookshare.dto.login.LoginDetails;
+import com.zedapps.bookshare.entity.book.Book;
 import com.zedapps.bookshare.entity.login.Review;
 import com.zedapps.bookshare.service.book.BookService;
 import jakarta.validation.Valid;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
@@ -29,14 +31,27 @@ public class BookController {
     }
 
     @GetMapping("/{id}")
-    public String book(@AuthenticationPrincipal LoginDetails loginDetails,
-                       @PathVariable Long id,
-                       ModelMap model) {
+    public String getBookPage(@AuthenticationPrincipal LoginDetails loginDetails,
+                              @PathVariable Long id,
+                              ModelMap model) {
 
         bookService.setupReferenceData(loginDetails, id, model);
         model.put("reviewDto", new BookReviewDto());
 
         return "app/book/book";
+    }
+
+    @GetMapping("/reviews/{id}")
+    public String getReviewPage(@PathVariable Long id,
+                                @RequestParam(defaultValue = "0") int page,
+                                ModelMap model) {
+
+        Book book = bookService.getBook(id);
+        Page<Review> reviews = bookService.getReviewsByBook(book, page);
+
+        model.put("reviews", reviews);
+
+        return "app/book/reviewList :: reviewList";
     }
 
     @PostMapping("/addReview")
