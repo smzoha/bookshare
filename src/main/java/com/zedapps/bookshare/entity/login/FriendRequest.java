10e0b714c9556804064248fe62ce46c36b5ab2fd
Shamah M Zoha <shamah.zoha@gmail.com>
2026-03-06 22:51:32 +0600

    fix: add separate sequence for friend request table

--- src/main/java/com/zedapps/bookshare/entity/login/FriendRequest.java
+++ src/main/java/com/zedapps/bookshare/entity/login/FriendRequest.java
@@ -16,7 +16,7 @@ import lombok.*;
 public class FriendRequest extends BaseConnection {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "connection_seq")
-    @SequenceGenerator(name = "connection_seq", sequenceName = "connection_seq", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "friend_req_seq")
+    @SequenceGenerator(name = "friend_req_seq", sequenceName = "friend_req_seq", allocationSize = 1)
     private Long id;
 }
