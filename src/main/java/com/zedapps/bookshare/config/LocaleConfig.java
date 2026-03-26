b5a05a75e75835846b3ebe92204fcf7ba2ba1b17
Shamah M Zoha <shamah.zoha@gmail.com>
2026-03-26 18:37:26 +0600

    feat: add configuration for resolving locale

---
+++ src/main/java/com/zedapps/bookshare/config/LocaleConfig.java
@@ -0,0 +1,42 @@
+package com.zedapps.bookshare.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.CookieLocaleResolver;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+
+import java.time.Duration;
+import java.util.Locale;
+
+/**
+ * @author smzoha
+ * @since 26/3/26
+ **/
+@Configuration
+public class LocaleConfig implements WebMvcConfigurer {
+
+    @Bean
+    public LocaleResolver localeResolver() {
+        CookieLocaleResolver resolver = new CookieLocaleResolver("lang");
+        resolver.setDefaultLocale(Locale.ENGLISH);
+        resolver.setCookieMaxAge(Duration.ofDays(30));
+
+        return resolver;
+    }
+
+    @Bean
+    public LocaleChangeInterceptor localeChangeInterceptor() {
+        LocaleChangeInterceptor interceptor = new LocaleChangeInterceptor();
+        interceptor.setParamName("lang");
+
+        return interceptor;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(localeChangeInterceptor());
+    }
+}
