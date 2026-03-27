2b0d9ae8b6baa4e1c344afb5f7335bd93887ef77
Shamah M Zoha <shamah.zoha@gmail.com>
2026-03-27 17:56:41 +0600

    chore: optimized imports

--- src/main/java/com/zedapps/bookshare/controller/login/app/PasswordResetController.java
+++ src/main/java/com/zedapps/bookshare/controller/login/app/PasswordResetController.java
@@ -17,7 +17,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import java.util.Objects;
 import java.util.Optional;
 
 /**
