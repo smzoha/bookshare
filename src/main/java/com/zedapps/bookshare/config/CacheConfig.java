ef2e092d61b2c86add95ca30da056796e9bd5fb9
Shamah M Zoha <shamah.zoha@gmail.com>
2026-04-14 00:48:05 +0600

    refactor: enabled caching in the project

--- src/main/java/com/zedapps/bookshare/config/CacheConfig.java
+++ src/main/java/com/zedapps/bookshare/config/CacheConfig.java
@@ -4,6 +4,7 @@ import com.github.benmanes.caffeine.cache.Caffeine;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.caffeine.CaffeineCache;
 import org.springframework.cache.support.SimpleCacheManager;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -15,6 +16,7 @@ import java.util.List;
  * @since 12/4/26
  **/
 @Configuration
+@EnableCaching
 public class CacheConfig {
 
     @Bean
