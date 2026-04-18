dea439de5c88375c29f6396631179a10f8e88ed8
Shamah M Zoha <shamah.zoha@gmail.com>
2026-04-18 20:53:01 +0600

    feat: implement ErrorResponseDto for returning validation errors

---
+++ src/main/java/com/zedapps/bookshare/dto/api/ErrorResponseDto.java
@@ -0,0 +1,27 @@
+package com.zedapps.bookshare.dto.api;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author smzoha
+ * @since 18/4/26
+ **/
+@Getter
+@Setter
+public class ErrorResponseDto {
+
+    private List<String> globalErrors;
+
+    private Map<String, List<String[]>> fieldErrors;
+
+    public ErrorResponseDto() {
+        this.globalErrors = new ArrayList<>();
+        this.fieldErrors = new HashMap<>();
+    }
+}
