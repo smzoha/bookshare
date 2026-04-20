8af9950f28b2428681314fd14342f07d208fd1a5
Shamah M Zoha <shamah.zoha@gmail.com>
2026-04-21 00:02:00 +0600

    refactor: add logging and additional exceptions to ApiExceptionHandler

--- src/main/java/com/zedapps/bookshare/controller/api/ApiExceptionHandler.java
+++ src/main/java/com/zedapps/bookshare/controller/api/ApiExceptionHandler.java
@@ -2,9 +2,12 @@ package com.zedapps.bookshare.controller.api;
 
 import com.zedapps.bookshare.dto.api.ErrorResponseDto;
 import jakarta.persistence.NoResultException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -14,6 +17,7 @@ import java.util.List;
  * @author smzoha
  * @since 20/4/26
  **/
+@Slf4j
 @RestControllerAdvice(basePackages = "com.zedapps.bookshare.controller.api")
 public class ApiExceptionHandler {
 
@@ -29,8 +33,24 @@ public class ApiExceptionHandler {
                 .body(new ErrorResponseDto(List.of("error.invalid.request")));
     }
 
+    @ExceptionHandler(AccessDeniedException.class)
+    public ResponseEntity<ErrorResponseDto> handleAccessDeniedException(AccessDeniedException ex) {
+        log.warn("ApiExceptionHandler: Access Denied", ex);
+
+        return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
+    }
+
+    @ExceptionHandler(AuthenticationException.class)
+    public ResponseEntity<ErrorResponseDto> handleAuthenticationException(AuthenticationException ex) {
+        log.warn("ApiExceptionHandler: Authentication Exception", ex);
+
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+    }
+
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ErrorResponseDto> handleGeneral(Exception ex) {
+        log.error("ApiExceptionHandler: Exception encountered: ", ex);
+
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
                 .body(new ErrorResponseDto(List.of("error.internal")));
     }
