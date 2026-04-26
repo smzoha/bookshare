e60d9c8874467c3f0e6d89a590921f7220960550
Shamah M Zoha <shamah.zoha@gmail.com>
2026-04-26 22:12:20 +0600

    feat: add tests for PasswordResetTokenRepository

---
+++ src/test/java/com/zedapps/bookshare/repository/PasswordRestTokenRepositoryTest.java
@@ -0,0 +1,64 @@
+package com.zedapps.bookshare.repository;
+
+import com.zedapps.bookshare.entity.login.PasswordResetToken;
+import com.zedapps.bookshare.repository.login.PasswordResetTokenRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * @author smzoha
+ * @since 26/4/26
+ **/
+@DataJpaTest
+@Testcontainers
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+public class PasswordRestTokenRepositoryTest {
+
+    @Container
+    @ServiceConnection
+    static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:17-alpine");
+
+    @Autowired
+    private PasswordResetTokenRepository passwordResetTokenRepository;
+
+    @Test
+    void getPasswordResetTokenByHashedSignature_returnToken() {
+        setupPasswordResetToken();
+
+        Optional<PasswordResetToken> token = passwordResetTokenRepository
+                .getPasswordResetTokenByHashedSignature("valid-signature");
+
+        assertTrue(token.isPresent());
+        assertEquals("test@test.com", token.get().getEmail());
+        assertEquals("valid-signature", token.get().getHashedSignature());
+        assertTrue(token.get().getExpiryTimestamp().isAfter(LocalDateTime.now()));
+        assertFalse(token.get().isInactive());
+
+        Optional<PasswordResetToken> invalidToken = passwordResetTokenRepository
+                .getPasswordResetTokenByHashedSignature("invalid-signature");
+
+        assertTrue(invalidToken.isEmpty());
+    }
+
+    private void setupPasswordResetToken() {
+        PasswordResetToken token = new PasswordResetToken();
+        token.setEmail("test@test.com");
+        token.setHashedSignature("valid-signature");
+        token.setInactive(false);
+        token.setGeneratedAt(LocalDateTime.now());
+        token.setExpiryTimestamp(LocalDateTime.now().plusMinutes(10));
+
+        passwordResetTokenRepository.save(token);
+    }
+}
