8b7f43e54c75f875ac29a0e62c07bc6b4d98beaa
Shamah M Zoha <shamah.zoha@gmail.com>
2026-06-27 00:13:39 +0600

    fix: missing dependency in ProfileApiControllerTest

--- src/test/java/com/zedapps/bookshare/controller/api/login/ProfileApiControllerTest.java
+++ src/test/java/com/zedapps/bookshare/controller/api/login/ProfileApiControllerTest.java
@@ -29,9 +29,7 @@ import java.util.Optional;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
-import static org.mockito.Mockito.never;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
